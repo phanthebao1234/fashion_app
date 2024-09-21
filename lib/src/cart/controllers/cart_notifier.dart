2f8a119c4dcdd2300124208aae61bcb21f7f0261
phanthebao1234 <phanthebao888@gmail.com>
2024-09-21 14:46:53 +0700

    Add to Cart

--- lib/src/cart/controllers/cart_notifier.dart
+++ lib/src/cart/controllers/cart_notifier.dart
@@ -1,9 +1,19 @@
 import 'package:fashion_app/common/services/storage.dart';
 import 'package:fashion_app/common/utils/environment.dart';
+import 'package:fashion_app/src/entrypoint/controllers/bottom_tab_notifier.dart';
+import 'package:fashion_app/src/products/controllers/colors_sizes_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 class CartNotifier with ChangeNotifier {
+  Function? refetchCount;
+
+  void setRefetchCount(Function? r) {
+    refetchCount = r;
+  }
+
   // Đây là định nghĩa một lớp tên là CartNotifier sử dụng mixin ChangeNotifier.
   // ChangeNotifier là một lớp do Flutter cung cấp giúp quản lý trạng thái và thông báo cho các listener khi có sự thay đổi
   int _qty =
@@ -48,16 +58,16 @@ class CartNotifier with ChangeNotifier {
   }
 
   Future<void> deleteCart(int id, void Function() refetch) async {
-     String? accessToken = Storage().getString('accessToken');
+    String? accessToken = Storage().getString('accessToken');
 
     try {
-      Uri url = Uri.parse(
-          '${Environment.appBaseUrl}/api/cart/delete/?id=$id');
+      Uri url = Uri.parse('${Environment.appBaseUrl}/api/cart/delete/?id=$id');
       final response = await http.delete(url, headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Token $accessToken'
       });
       if (response.statusCode == 204) {
+        refetchCount!();
         refetch();
         clearSelected();
       }
@@ -79,6 +89,36 @@ class CartNotifier with ChangeNotifier {
       if (response.statusCode == 200) {
         refetch();
         clearSelected();
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  Future<void> addToCart(String data, BuildContext context) async {
+    String? accessToken = Storage().getString('accessToken');
+
+    try {
+      Uri url = Uri.parse('${Environment.appBaseUrl}/api/cart/add/');
+      final response = await http.post(
+        url,
+        body: data,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Token $accessToken'
+        },
+      );
+      if (response.statusCode == 201) {
+        print("add to cart successeccfully");
+        // refetch count
+        refetchCount!();
+
+        context.read<ColorsSizesNotifier>().setSizes('');
+        context.read<ColorsSizesNotifier>().setColors('');
+
+        // navigate to the cart
+        context.read<TabIndexNotifier>().setIndex(3);
+        context.go('/home');
       }
     } catch (e) {
       debugPrint(e.toString());
