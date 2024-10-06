57607c3d0f8b4943127edfcd8387e507e6a0b568
phanthebao1234 <phanthebao888@gmail.com>
2024-10-06 23:38:58 +0700

    add function update isRead notification

--- lib/src/notification/controllers/notification_notifier.dart
+++ lib/src/notification/controllers/notification_notifier.dart
@@ -1,9 +1,47 @@
+import 'package:fashion_app/common/services/storage.dart';
+import 'package:fashion_app/common/utils/environment.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class NotificationNotifier with ChangeNotifier {
   Function? refetchCount;
 
   void setRefetchCount(Function? r) {
     refetchCount = r;
+  }
+
+  Function? refetch;
+
+  void setRefetch(Function? r) {
+    refetch = r;
+  }
+
+  int _orderId = 0;
+
+  int get orderId => _orderId;
+
+  void setOrderId(int id) {
+    _orderId = id;
+    markAsRead(id);
+    notifyListeners();
+  }
+
+  Future<void> markAsRead(int id) async {
+    String? accessToken = Storage().getString('accessToken');
+
+    try {
+      Uri url = Uri.parse(
+          '${Environment.appBaseUrl}/api/notification/update/?id=$id');
+      final response = await http.patch(url, headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token $accessToken'
+      });
+      if (response.statusCode == 200) {
+        refetch!();
+        refetchCount!();
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 }
